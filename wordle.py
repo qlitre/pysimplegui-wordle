58fdcc8ac9f135ea84df41c02e9b27bc265336bb
qlitre <kuri0403@gmail.com>
2022-02-02 00:08:03 +0900

    answerリストを本家に合わせた

--- wordle.py
+++ wordle.py
@@ -18,34 +18,22 @@ class Wordle:
 
     def is_word_in_word_list(self, word: str):
         """単語がword listの中に存在していたらTrue"""
-        if word in self.word_list:
-            return True
-        else:
-            return False
+        return word in self.word_list
 
     def is_word_collect(self, word: str):
         """
         5文字の単語が答えに合っていればTrue
         """
-        if word == self.answer:
-            return True
-        else:
-            return False
+        return word == self.answer
 
     def is_char_in_answer(self, char: str):
         """
         英語一文字が答えに含まれていたらTrue
         """
-        if char in self.answer:
-            return True
-        else:
-            return False
+        return char in self.answer
 
     def is_char_right_position(self, char: str, pos: int):
         """
         英語一文字が答えと照らし合わせて位置が合っていたらTrue
         """
-        if self.answer[pos] == char:
-            return True
-        else:
-            return False
+        return self.answer[pos] == char
